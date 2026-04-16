342a020259385ff3a419a5d4b5b070583346eac6
Emmanuel Alexis Morfin Guerra <136378985+Alexis1221345@users.noreply.github.com>
2026-04-16 16:45:35 -0600

    Nuevos Cambios Internos

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Jenifer\Downloads\pillcare_flutter\pillcare"
+export "FLUTTER_ROOT=C:\dev\flutter_windows_3.38.8-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\alexi\StudioProjects\pastillero-"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
